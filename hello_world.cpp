da821f82c6ca0a22ca404a70041434a8e0004942
matveybaykalov <m.baykalov@mail.ru>
2021-04-09 20:53:53 +0300

    Added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string name;
-    std::getline(std::cin, name);
+    std::getline(std::cin, name); //здесь запрашивается имя у пользователя
     std::cout << "Hello, World from " << name << std::endl;
     return 0;
 }
