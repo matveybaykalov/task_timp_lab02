e9d960e787d4782aa279da63beed27323851537d
matveybaykalov <m.baykalov@mail.ru>
2021-04-09 16:07:24 +0300

    Change program hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    string name;
+    getline(cin, name);
+    cout << "Hello, World from " << name << endl;
     return 0;
 }
+
